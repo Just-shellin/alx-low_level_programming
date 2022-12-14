300a53b56d41f65b5c59bbf36fe6de65dad0a707
Just-shellin <mulingashellinmwikali@gmail.com>
2022-12-14 08:32:36 -0800

    2-print_alphabet_x10.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints 10 times the alphabet
+ * in lowercase, followed by a new line
+ *
+ * Return: 0;
+ */
+void print_alphabet_x10(void)
+{
+	int i, tot;
+
+	tot = 0
+
+	while (tot < 10)
+	{
+		for (i = 'a'; i <= 'z'; i++)
+		{
+			_putchar(n);
+		}
+		tot++;
+		_putchar('\n');
+	}
+}
