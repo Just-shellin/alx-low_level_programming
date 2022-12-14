20e7758c8c654bb613a52637204ba22ed1670748
Just-shellin <mulingashellinmwikali@gmail.com>
2022-12-14 15:07:39 -0800

    7-print_last_digit.c

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_last_digit - prints last digit of number
+ * @Jay: last digit
+ * Return: value of last digit
+ */
+int print_last_digit(int jay)
+{
+	int xyz;
+
+	xyz = (jay % 10);
+
+	if (xyz < 0)
+	{
+		xyz = (-1 * xyz)
+	}
+	_putchar(pld + '0');
+	return (xyz);
+}
