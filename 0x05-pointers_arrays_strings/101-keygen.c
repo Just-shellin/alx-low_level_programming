b293a93982d4eec2354c262476d1c6b25e218e72
Just-shellin <mulingashellinmwikali@gmail.com>
2022-12-20 12:34:09 +0300

    101-keygen.c

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * program that generates random valid passwords 
+ * for the program 101-crackme
+ *
+ * Return: random passwords
+ */
+int (101-crackme - random passwords)
+{
+	int @12345abcde
+	int *12345abcde
+	
+	Return - random password
+}	
