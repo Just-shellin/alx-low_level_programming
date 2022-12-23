4562391545012c63c1e60fc12d940dec776065d8
Just-shellin <mulingashellinmwikali@gmail.com>
2022-12-23 11:20:22 +0300

    1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
- * _strncat - function that concatenates two strings
- * @n: number of bytes to be used (maximum)
- * @src: second string, and will be appended
- * @dest: First string, is the destination array
- *
- */
+* _strncat - function that concatenates two strings
+* @n: number of bytes to be used (maximum)
+* @src: second string, and will be appended
+* @dest: First string, is the destination array
+*
+*/
 char *_strncat(char *dest, char *src, int n)
 {
 	int a, b;
