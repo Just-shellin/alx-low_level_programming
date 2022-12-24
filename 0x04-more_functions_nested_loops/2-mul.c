d869b17690923bc609d6879fa887998d2f1f4be8
Just-shellin <mulingashellinmwikali@gmail.com>
2022-12-24 14:02:43 +0300

    2-mul.c

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -4,6 +4,8 @@
  * mul - function that multiplies two integers
  * @a: first integer to be multiplied 
  * @b: other integer to be multiplied
+ *
+ * Return: The result of multiplication between a param and b param
  */
 int mul(int a, int b)
 {
