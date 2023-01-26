05c4e4d475bb9e35fa2440e70b671bae6e1af899
Just-shellin <mulingashellinmwikali@gmail.com>
2023-01-26 13:57:23 +0300

    0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 /**
- * _puts_recursion - function that prints a string 
+ * _puts_recursion - function that prints a string
  * followed by a new line.
  * @s: string to be printed
  *
